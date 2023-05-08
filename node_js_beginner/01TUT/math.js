63a71e04dd314464ca4f146d6f84c6c2ae38ec50
sunil parmar <sunil04ce@gmail.com>
2023-05-08 09:08:02 +0530

    use of exports, read, write, append, rename file using fs

---
+++ node_js_beginner/01TUT/math.js
@@ -0,0 +1,13 @@
+exports.add = (a, b) => a + b;
+exports.substract = (a, b) => a - b;
+exports.multiply = (a, b) => a * b;
+exports.divide = (a, b) => a / b;
+
+
+
+// const add = (a, b) => a + b;
+// const substract = (a, b) => a - b;
+// const multiply = (a, b) => a * b;
+// const divide = (a, b) => a / b;
+
+// module.exports = { add, substract, multiply, divide };
--- node_js_beginner/01TUT/server.js
+++ node_js_beginner/01TUT/server.js
@@ -1,18 +1,25 @@
 
+
 const os = require('os');
 const path = require('path');
 
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
+const { add, substract, multiply, divide } = require('./math');
+console.log(add(5, 4));
+console.log(substract(5, 4));
+console.log(multiply(5, 4));
+console.log(divide(5, 4));
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
 
-console.log("dirname : ", path.dirname(__filename));
-console.log("basename : ", path.basename(__filename));
-console.log("extname : ", path.extname(__filename));
+// console.log(__dirname);
+// console.log(__filename);
 
-console.log("parse : ", path.parse(__filename));
-//console.log('Hello world!');
+// console.log("dirname : ", path.dirname(__filename));
+// console.log("basename : ", path.basename(__filename));
+// console.log("extname : ", path.extname(__filename));
+
+// console.log("parse : ", path.parse(__filename));
+
 //console.log(global);
