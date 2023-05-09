3dd01f6a8bca6b10e798395d98ea22739addeeca
sunil parmar <sunil04ce@gmail.com>
2023-05-09 22:40:16 +0530

    installed package date-fns, uuid, use of scripts to start,dev project

---
+++ node_js_beginner/03TUT/index.js
@@ -0,0 +1,7 @@
+// console.log('testing! nodemon');
+
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
+console.log(uuid());
