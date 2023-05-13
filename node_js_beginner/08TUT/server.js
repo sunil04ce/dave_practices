aafcc66c0baf229409aa89777ea16f7bae0cda61
sunil parmar <sunil04ce@gmail.com>
2023-05-13 14:33:09 +0530

    implemented routes for employee

---
+++ node_js_beginner/08TUT/server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+// Cors : Cross origin resource sharing
+const whitelist = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+// build-in middleware to handle urlencoded data
+// in other words, form data:
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+// build-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use('/', express.static(path.join(__dirname, './public')));
+app.use('/subdir', express.static(path.join(__dirname, './public')));
+
+// routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
+
+// Route Handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempt to load hello.html');
+    next();
+}, (req, res) => {
+    res.send('Hello world!');
+});
+
+// chaining route handlers
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+};
+
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+};
+
+const three = (req, res, next) => {
+    console.log('three');
+    res.send('Finished!');
+};
+
+app.get('/chain(.html)?', [one, two, three]);
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 not found" });
+    } else {
+        res.type('txt').send("404 not found");
+    }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
+
+
+
