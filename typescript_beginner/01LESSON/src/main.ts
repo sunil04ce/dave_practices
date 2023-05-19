d6dfb77bee8fce73ef40b690831ae770e2ad1b71
sunil parmar <sunil04ce@gmail.com>
2023-05-19 09:29:53 +0530

    use of union type, regExp

--- typescript_beginner/01LESSON/src/main.ts
+++ typescript_beginner/01LESSON/src/main.ts
@@ -5,9 +5,26 @@ let a: number = 12;
 let b: string = "6";
 let c: number = 2;
 
-console.log(a / b);
-console.log(c * b);
+// console.log(a / b);
+// console.log(c * b);
 
 let myName: string = "Jack";
 myName = "Mark";
 console.log(myName);
+
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any; // string | number
+
+meaningOfLife = 42;
+isLoading = true;
+album = 1985; // "Micheal";
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
