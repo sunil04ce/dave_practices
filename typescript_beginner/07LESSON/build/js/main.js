b389dc479d581e000eea8c2771aa6299f0b57306
sunil parmar <sunil04ce@gmail.com>
2023-05-24 23:28:39 +0530

    use of interface

---
+++ typescript_beginner/07LESSON/build/js/main.js
@@ -0,0 +1,46 @@
+"use strict";
+// Index Signatures
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -10,
+    Job: 50,
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Pizza"]);
+let prop = "Pizza";
+console.log(todaysTransactions[prop]);
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+};
+console.log(todaysNet(todaysTransactions));
+// todaysTransactions.Pizza = 40;
+console.log(todaysNet(todaysTransactions));
+console.log(todaysTransactions["Jack"]);
+const student = {
+    name: "Doug",
+    GPA: 4.8,
+    classes: [100, 200],
+};
+//console.log(student.test);
+for (const key in student) {
+    console.log(`${key} : ${student[key]}`);
+}
+Object.keys(student).map((key) => {
+    console.log(student[key]);
+});
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key} : ${student[key]}`);
+};
+logStudentKey(student, "GPA");
+const monthlyIncomes = {
+    salary: 500,
+    bonus: 100,
+    sidehustle: 250,
+};
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue]);
+}
