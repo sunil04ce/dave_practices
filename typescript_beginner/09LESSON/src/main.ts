e410ba0287bdd03c54fb660a1133f04b2c141b6a
sunil parmar <sunil04ce@gmail.com>
2023-05-27 21:06:15 +0530

    use of required, record, readonly

--- typescript_beginner/09LESSON/src/main.ts
+++ typescript_beginner/09LESSON/src/main.ts
@@ -24,3 +24,33 @@ const assign1: Assignment = {
 
 console.log(updateAssignment(assign1, { grade: 95 }));
 const assignGraded: Assignment = updateAssignment(assign1, { grade: 95 });
+
+// Required and Readonly
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+  // send to database etc.
+  return assign;
+};
+
+const assignVerified: Readonly<Assignment> = {
+  ...assignGraded,
+  verified: true,
+};
+
+// assignVerified.grade = 100;
+recordAssignment({ ...assignGraded, verified: true });
+
+// Record
+const hexColorMap: Record<string, string> = {
+  red: "FF0000",
+  green: "00FF00",
+  blue: "0000FF",
+};
+
+type Students = "Sara" | "Kelly";
+type LetterGrades = "A" | "B" | "C" | "D" | "U";
+
+const finalGrades: Record<Students, LetterGrades> = {
+  Sara: "B",
+  Kelly: "U",
+  // Kelly: "Z",
+};
