e2bf097f3791f4e6c1a1612d72c55fd5b27d7d8c
sunil parmar <sunil04ce@gmail.com>
2023-05-31 23:17:33 +0530

    changes in ListTemplate class

--- typescript_beginner/11LESSON/src/templates/ListTemplate.ts
+++ typescript_beginner/11LESSON/src/templates/ListTemplate.ts
@@ -5,3 +5,39 @@ interface DOMList {
   clear(): void;
   render(fullList: FullList): void;
 }
+
+export default class ListTemplate implements DOMList {
+  ul: HTMLUListElement;
+
+  clear(): void {
+    this.ul.innerHTML = "";
+  }
+  render(fullList: FullList): void {
+    this.clear();
+    fullList.list.forEach((item) => {
+      const li = document.createElement("li") as HTMLElement;
+      li.className = "item";
+
+      const check = document.createElement("input") as HTMLInputElement;
+      check.type = "checkbox";
+      check.id = item.id;
+      check.tabIndex = 0;
+      check.checked = item.checked;
+      li.append(check);
+
+      check.addEventListener("change", () => {
+        item.checked = !item.checked;
+        fullList.save();
+      });
+
+      const label = document.createElement("label") as HTMLLabelElement;
+      label.htmlFor = item.id;
+      label.textContent = item.item;
+      li.append(label);
+    });
+  }
+
+  private constructor() {
+    this.ul = document.getElementById("listItems") as HTMLUListElement;
+  }
+}
