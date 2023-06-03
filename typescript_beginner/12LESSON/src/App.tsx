bd192ccefa8075fc6c899a82fabeffead58bbf9c
sunil parmar <sunil04ce@gmail.com>
2023-06-03 08:15:11 +0530

    added List component

--- typescript_beginner/12LESSON/src/App.tsx
+++ typescript_beginner/12LESSON/src/App.tsx
@@ -3,6 +3,7 @@ import Heading from "./components/Heading";
 import { Section } from "./components/Section";
 import Counter from "./components/Counter";
 import { useState } from "react";
+import List from "./components/List";
 
 function App() {
   const [count, setCount] = useState<number>(1);
@@ -11,6 +12,10 @@ function App() {
       <Heading title={"Hello"} />
       <Section title={"Different Title"}>This is subsection</Section>
       <Counter setCount={setCount}>Count is {count}</Counter>
+      <List
+        items={["Coffee", "Tacos", "Code"]}
+        render={(item: string) => <span className="bold">{item}</span>}
+      ></List>
     </>
   );
 }
