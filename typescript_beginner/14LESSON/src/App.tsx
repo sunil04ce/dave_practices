0b54126eef485f869258fd8b3db3e9a689d9792a
sunil parmar <sunil04ce@gmail.com>
2023-06-05 23:13:05 +0530

    before use of useContext hook

---
+++ typescript_beginner/14LESSON/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <>
+      <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+    </>
+  );
+}
+
+export default App;
