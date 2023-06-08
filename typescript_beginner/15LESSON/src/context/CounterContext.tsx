d6b9e78a25c6e1dcefa3c96a70d4ebb6f5b3aa8b
sunil parmar <sunil04ce@gmail.com>
2023-06-08 22:46:21 +0530

    exported useCounter, useCounterText

--- typescript_beginner/15LESSON/src/context/CounterContext.tsx
+++ typescript_beginner/15LESSON/src/context/CounterContext.tsx
@@ -4,6 +4,7 @@ import {
   ReactNode,
   createContext,
   useCallback,
+  useContext,
   useReducer,
 } from "react";
 
@@ -82,4 +83,34 @@ export const CounterProvider = ({
       {children}
     </CounterContext.Provider>
   );
+};
+
+type UseCounterHookType = {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+};
+
+export const useCounter = (): UseCounterHookType => {
+  const {
+    state: { count },
+    increment,
+    decrement,
+  } = useContext(CounterContext);
+
+  return { count, increment, decrement };
+};
+
+type UseCounterTextHookType = {
+  text: string;
+  handleTextInput: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const useCounterText = (): UseCounterTextHookType => {
+  const {
+    state: { text },
+    handleTextInput,
+  } = useContext(CounterContext);
+
+  return { text, handleTextInput };
 };
