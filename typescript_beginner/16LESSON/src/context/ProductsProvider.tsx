f6d6ee6502d2b0df9edc409586903c784a3ba739
sunil parmar <sunil04ce@gmail.com>
2023-06-09 09:32:10 +0530

    added ProductsProvider

---
+++ typescript_beginner/16LESSON/src/context/ProductsProvider.tsx
@@ -0,0 +1,30 @@
+import { createContext } from "react";
+
+export type ProductType = {
+  sku: string;
+  name: string;
+  price: number;
+};
+
+const initState: ProductType[] = [
+  {
+    sku: "item0001",
+    name: "Widget",
+    price: 9.99,
+  },
+  {
+    sku: "item0002",
+    name: "Premium Widget",
+    price: 19.99,
+  },
+  {
+    sku: "item0003",
+    name: "Deluxe Widget",
+    price: 29.99,
+  },
+];
+
+export type UseProductsContextType = { products: ProductType[] };
+const initContextState: UseProductsContextType = { products: [] };
+
+const ProductsContext = createContext<UseProductsContextType>(initContextState);
