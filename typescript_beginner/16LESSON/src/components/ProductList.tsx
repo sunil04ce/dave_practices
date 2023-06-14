f2bdf1734994214d3432f13297dc6372ef7d2666
sunil parmar <sunil04ce@gmail.com>
2023-06-14 22:39:46 +0530

    created useProducts hook, changes in ProductsProvider

--- typescript_beginner/16LESSON/src/components/ProductList.tsx
+++ typescript_beginner/16LESSON/src/components/ProductList.tsx
@@ -1,4 +1,22 @@
+import useCart from "../hooks/useCart";
+import useProducts from "../hooks/useProducts";
+import { UseProductsContextType } from "../context/ProductsProvider";
+import { ReactElement } from "react";
+
 const ProductList = () => {
+  const { dispatch, REDUCER_ACTIONS, cart } = useCart();
+  const { products } = useProducts();
+
+  let pageContent: ReactElement | ReactElement[] = <p>Loading...</p>;
+  if (products?.length) {
+    pageContent = products.map((product) => {
+      const inCart: boolean = cart.some((item) => item.sku === product.sku);
+      return(
+
+      );
+    });
+  }
+
   return <div>ProductList</div>;
 };
 
