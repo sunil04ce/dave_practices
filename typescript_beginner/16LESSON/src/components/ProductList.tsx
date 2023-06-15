51e8390d3ea14d4ec3943f1ae6884ddc1aaccfd3
sunil parmar <sunil04ce@gmail.com>
2023-06-15 22:34:28 +0530

    displayed Products

--- typescript_beginner/16LESSON/src/components/ProductList.tsx
+++ typescript_beginner/16LESSON/src/components/ProductList.tsx
@@ -2,6 +2,7 @@ import useCart from "../hooks/useCart";
 import useProducts from "../hooks/useProducts";
 import { UseProductsContextType } from "../context/ProductsProvider";
 import { ReactElement } from "react";
+import Product from "./Product";
 
 const ProductList = () => {
   const { dispatch, REDUCER_ACTIONS, cart } = useCart();
@@ -11,13 +12,21 @@ const ProductList = () => {
   if (products?.length) {
     pageContent = products.map((product) => {
       const inCart: boolean = cart.some((item) => item.sku === product.sku);
-      return(
-
+      return (
+        <Product
+          key={product.sku}
+          product={product}
+          dispatch={dispatch}
+          REDUCER_ACTIONS={REDUCER_ACTIONS}
+          inCart={inCart}
+        />
       );
     });
   }
 
-  return <div>ProductList</div>;
+  const content = <main className="main main--products">{pageContent}</main>;
+
+  return content;
 };
 
 export default ProductList;
